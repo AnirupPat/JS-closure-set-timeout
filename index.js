0d732fac92a4597b5de157bf0a7817c30f75c033
Anirup Patnaik <anirup049@gmail.com>
2021-07-06 16:12:23 +0530

    Closures with var, let and IIFE

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Working!");
+// basic eg. of closure
+
+function fun() {
+  var i = 1;
+  setTimeout(() => {
+    console.log(i);
+  }, 3000);
+}
+console.log("Hello world!");
+
+fun();
+
+// var with closure.. use let to get the exoected output
+
+function fun2() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
+}
+
+fun2();
+
+// using IIFE to make closure to work
+
+function fun2() {
+  for (var i = 1; i <= 5; i++) {
+    (function (x) {
+      setTimeout(function () {
+        console.log(x);
+      }, x * 1000);
+    })(i);
+  }
+}
+
+fun2();
